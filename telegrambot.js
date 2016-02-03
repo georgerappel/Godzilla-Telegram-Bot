1704c08e39a7d6fa1340e236fd00e2ee798a7814
George Rappel <george.concei@hotmail.com>
2016-02-03 13:17:42 -0200

    Update Travis.yml and Env variables

---
+++ telegrambot.js
@@ -0,0 +1,73 @@
+var debug = true;
+var quietList = '';
+
+var Bot = require('node-telegram-bot');
+var bot;
+
+function start() {
+	bot= new Bot({
+		token: process.env.TELEGRAM_TOKEN
+	  })
+	  .enableAnalytics(process.env.TELEGRAM_ANALYTICS)
+	  .on('message', handleMessage)
+	  .on('error', function (message) {
+		  console.error('It seems like we lost the connection...');
+		  throw new Error('Telegram error:', message);
+	  })
+	  .start();
+}
+
+function handleMessage(message) {
+	console.log(message);
+	if (message && message.text && /^\/[a-z]+/.test(message.text)) {
+		var parsed = /^\/([a-z]+)\s?(.*)/.exec(message.text);
+		if (!parsed) { return; }
+		var command = parsed[1];
+		var param = parsed[2];
+		try {
+			if (debug) {
+				console.log('received /' + command + ' ' + param);
+			}
+			var cmd = require('./commands/' + command.toLowerCase());
+			if ((typeof cmd !== 'function') && cmd.telegram) {
+				if(debug){ console.log('not a function'); }
+				cmd = cmd.telegram;
+			}
+			var answer = cmd(message.from, message.chat, param ? param.trim() : '', bot);
+			if (answer && quietList.indexOf(message.chat.id) === -1) {
+				bot.sendMessage({
+					chat_id: message.chat.id,
+			        text: answer,
+			        reply_to_message_id: message.message_id,
+			        disable_web_page_preview: true
+				}, function (err, msg) {
+					if (debug) { console.log('executed /' + command); }
+					if(err) { console.log(err); }
+				});
+			}
+		} catch (e) {
+			if (debug) {
+				console.log(e);
+			}
+		}
+	} else if (message && message.text && (message.text.toLowerCase().indexOf("@nanozilla_bot") || message.text.toLowerCase().indexOf("Godzilla")) ){
+		console.log("Fui citado");
+
+	}
+}
+
+function sendMessage(destination, text) {
+	console.log('sending message to ', destination);
+	bot.sendMessage({
+		chat_id: destination,
+        text: text,
+        disable_web_page_preview: true
+	});
+}
+
+// Export the functions for global public acess
+module.exports = {
+	start: start,
+	bot: bot,
+	sendMessage: sendMessage	
+};
